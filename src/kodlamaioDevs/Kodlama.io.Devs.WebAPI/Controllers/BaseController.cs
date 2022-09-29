94e7b1dbe67e9b7b30079ee42cf302385bffa8fc
Yusuf Çırak <yusuf10901@hotmail.com>
2022-09-29 22:35:38 +0300

    BaseController refactor

--- src/kodlamaioDevs/Kodlama.io.Devs.WebAPI/Controllers/BaseController.cs
+++ src/kodlamaioDevs/Kodlama.io.Devs.WebAPI/Controllers/BaseController.cs
@@ -7,5 +7,5 @@ public class BaseController:ControllerBase
 {
     private IMediator? _mediator;
 
-    protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>()!;
 }
